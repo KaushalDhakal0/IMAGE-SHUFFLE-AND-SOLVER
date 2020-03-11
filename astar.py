c414f75f32c18f699670967d2e1a663c8eab2614
KaushalDhakal0 <kaushaldhakal90@gmail.com>
2020-03-11 21:28:23 +0545

    Update astar.py

--- astar.py
+++ astar.py
@@ -4,10 +4,8 @@ def Astar(state):
     while frontier:
         frontier.sort(key=lambda x: x[1])
         s = frontier[0]
-        # print("Top of the frontier"+str(s))
         frontier.remove(s)
         if goalState(s[0]):
-            # print("Goal state "+str(s))
             index = s[2][:]
             break
         explored.append(s[0])
